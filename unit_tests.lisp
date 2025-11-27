dfa8a79953494cfcf4c5db4eb39a2398f417265d
E2119 <126727766+E2119@users.noreply.github.com>
2025-11-27 00:31:48 -0700

    Add unit tests for set-member and set-diff functions

--- unit_tests.lisp
+++ unit_tests.lisp
@@ -3,6 +3,25 @@
 
 ;; Problem: 1
 
+(defun test-set-member ()
+  (assert (eq (set-member '(1 2 3) 2) t)
+          nil "Test 1 Failed")
+
+  (assert (eq (set-member '(1 2 3) 99) nil)
+          nil "Test 2 Failed")
+
+  (assert (eq (set-member nil 5) nil)
+          nil "Test 3 Failed")
+
+  (assert (eq (set-member '(a b c) 'b) t)
+          nil "Test 4 Failed")
+
+  (assert (eq (set-member '(a b c) 'z) nil)
+          nil "Test 5 Failed")
+
+  (format t "SET-MEMBER tests passed.~%"))
+
+(test-set-member)
 
 ;; Problem: 2
 
@@ -41,7 +60,25 @@
 
 ;; Problem: 4
 
+(defun test-set-diff ()
+  (assert (equal (set-diff '(1 2) '(2 4)) '(1))
+          nil "Test 1 Failed")
 
+  (assert (equal (set-diff '(1 2 3) '(3 1)) '(2))
+          nil "Test 2 Failed")
+
+  (assert (equal (set-diff '(a b c) '(b)) '(a c))
+          nil "Test 3 Failed")
+
+  (assert (equal (set-diff '(1 2 3) nil) '(1 2 3))
+          nil "Test 4 Failed")
+
+  (assert (equal (set-diff nil '(1 2 3)) nil)
+          nil "Test 5 Failed")
+
+  (format t "SET-DIFF tests passed.~%"))
+
+(test-set-diff)
 
 
 ;; Problem: 5
