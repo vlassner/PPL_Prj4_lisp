5a0a58f2041b27b55dafaf139890d6e738af83d6
Jonathon Pedraza <50535156+Pedraza7@users.noreply.github.com>
2025-12-02 20:04:42 -0700

    Added tests for set-union, boolean-xor, and boolean-eval

--- unit_tests.lisp
+++ unit_tests.lisp
@@ -24,7 +24,21 @@
 (test-set-member)
 
 ;; Problem: 2
+(defun test-set-union ()
+  (assert (equal (set-union (list 0 1 2 3) (list 4 5 6 7)) (list 3 2 1 0 4 5 6 7))
+    nil "Test 1 Failed")
+  (assert (equal (set-union (list 0 1 2 3) (list 4 1 6 3)) (list 2 0 4 1 6 3))
+    nil "Test 2 Failed")
+  (assert (equal (set-union (list 0 1 2 3 4 5 6 7) (list 9 4 10 6 11)) (list 7 5 3 2 1 0 9 4 10 6 11))
+    nil "Test 3 Failed")
+  (assert (equal (set-union (list 0 1 2 3 4) (list 0 1 2 3 4)) (list 0 1 2 3 4))
+    nil "Test 4 Failed")
+  (assert (equal (set-union (list 0 1 2 3) (list 100 218 2 27)) (list 3 1 0 100 218 2 27))
+    nil "Test 5 Failed")
 
+  (format t "SET-UNION tests passed.~%"))
+
+(test-set-union)
 
 
 ;; Problem: 3
@@ -82,7 +96,19 @@
 
 
 ;; Problem: 5
+(defun test-boolean-xor ()
+  (assert (equal (boolean-xor t t) nil)
+    nil "Test 1 Failed")
+  (assert (equal (boolean-xor t nil) t)
+    nil "Test 2 Failed")
+  (assert (equal (boolean-xor nil t) t)
+    nil "Test 3 Failed")
+  (assert (equal (boolean-xor nil nil) nil)
+    nil "Test 4 Failed")
 
+  (format t "BOOLEAN-XOR tests passed.~%"))
+
+(test-boolean-xor)
 
 
 ;; Problem: 6
@@ -108,6 +134,42 @@
 
 
 ;; Problem: 8
+(defun test-boolean-eval()
+  (assert (equal (boolean-eval '(and t nil)) nil)
+    nil "Test 1 Failed")
+  (assert (equal (boolean-eval '(and t t)) t)
+    nil "Test 2 Failed")
+  (assert (equal (boolean-eval '(or t nil)) t)
+    nil "Test 3 Failed")
+  (assert (equal (boolean-eval '(or nil nil)) nil)
+    nil "Test 4 Failed")
+  (assert (equal (boolean-eval '(not t)) nil)
+    nil "Test 5 Failed")
+    (assert (equal (boolean-eval '(not nil)) t)
+    nil "Test 6 Failed")
+  (assert (equal (boolean-eval '(xor t nil)) t)
+    nil "Test 7 Failed")
+    (assert (equal (boolean-eval '(xor t t)) nil)
+    nil "Test 8 Failed")
+  (assert (equal (boolean-eval '(implies t nil)) nil)
+    nil "Test 9 Failed")
+  (assert (equal (boolean-eval '(implies nil nil)) t)
+    nil "Test 10 Failed")
+  (assert (equal (boolean-eval '(iff t nil)) nil)
+    nil "Test 11 Failed")
+  (assert (equal (boolean-eval '(iff t t)) t)
+    nil "Test 12 Failed")
+  (assert (equal (boolean-eval '(and t (or t nil))) t)
+    nil "Test 13 Failed")
+  (assert (equal (boolean-eval '(xor (and t nil) (not nil))) t)
+    nil "Test 14 Failed")
+  (assert (equal (boolean-eval '(and (iff t nil) (or t nil))) nil)
+    nil "Test 15 Failed")
+  (assert (equal (boolean-eval '(or (implies t nil) (and t (or t nil)))) t)
+    nil "Test 16 Failed")
 
+  (format t "BOOLEAN-EVAL tests passed.~%"))
+
+(test-boolean-eval)
 
 ;; Problem: 9
